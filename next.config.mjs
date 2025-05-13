d88e4648c0cf944f9cc250b59312d111e13419ce
Hardik Amin <hardikamin@Hardiks-MacBook-Pro.local>
2025-05-13 18:26:07 -0500

    Added conditional config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/house-style-detector' : '',
+  assetPrefix: isProd ? '/house-style-detector/' : '',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
