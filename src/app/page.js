a8e8649b075e077ad037fa79a10532b5d8ac526e
Hardik Amin <hardikamin@Hardiks-MacBook-Pro.local>
2025-05-13 18:13:53 -0500

    Added SVG icons and configured Inter fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import { UploadIcon, ButtonCheckIcon, ButtonCrossIcon } from "../components/Icons";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-5xl font-bold mb-4">Tailwind is Working 🎉</h1>
-      <p className="text-lg">This is a test page for Tailwind CSS.</p>
+      <h1 className="text-5xl font-bold mb-4">Icon Test</h1>
+      <div className="flex gap-4">
+        <UploadIcon className="w-12 h-12" />
+        <ButtonCheckIcon className="w-12 h-12" />
+        <ButtonCrossIcon className="w-12 h-12" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Icons.js
@@ -0,0 +1,31 @@
+import AlertIcon from "../../public/icons/alert.svg";
+import ButtonCheckIcon from "../../public/icons/buttoncheck.svg";
+import ButtonCrossIcon from "../../public/icons/buttoncross.svg";
+import CollapseIcon from "../../public/icons/collapse.svg";
+import CursorIcon from "../../public/icons/cursor.svg";
+import ExitCrossIcon from "../../public/icons/exitcross.svg";
+import ExpandIcon from "../../public/icons/expand.svg";
+import FileIcon from "../../public/icons/file.svg";
+import GlobeIcon from "../../public/icons/globe.svg";
+import LabelIcon from "../../public/icons/label.svg";
+import NextIcon from "../../public/icons/next.svg";
+import UploadIcon from "../../public/icons/upload.svg";
+import VercelIcon from "../../public/icons/vercel.svg";
+import WindowIcon from "../../public/icons/window.svg";
+
+export {
+  AlertIcon,
+  ButtonCheckIcon,
+  ButtonCrossIcon,
+  CollapseIcon,
+  CursorIcon,
+  ExitCrossIcon,
+  ExpandIcon,
+  FileIcon,
+  GlobeIcon,
+  LabelIcon,
+  NextIcon,
+  UploadIcon,
+  VercelIcon,
+  WindowIcon,
+};
